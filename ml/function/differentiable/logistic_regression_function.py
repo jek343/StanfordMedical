45a1a5777913df7a833880c8bf755c67af820dc5
pgh36 <pgh36@cornell.edu>
2019-09-23 20:00:46 -0400

    deleted useless files

--- ml/function/differentiable/logistic_regression_function.py
+++
@@ -1,9 +0,0 @@
-import ml.function.differentiable.sigmoid as sigmoid
-import numpy as np
-
-def f(x, theta):
-    return sigmoid.f(np.dot(x, theta))
-
-def grad_theta_f(x, theta):
-    f_x = f(x, theta)
-    return f_x * (1 - f_x) * x
