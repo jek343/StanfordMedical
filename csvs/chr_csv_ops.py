a6acc47c0a6b04058250c286f68824f7be3df132
Cora Wu <cjw322@cornell.edu>
2019-10-06 23:08:35 -0400

    Added linear regression file & created separate csv_ops specifically for CHR data

---
+++ csvs/chr_csv_ops.py
@@ -0,0 +1,54 @@
+import csv
+import os
+import peter_csv_ops as peter_csv_ops
+import numpy as np
+
+def get_clean_fields(data_dict, field_names, min_appearance_percentage):
+    field_counts = np.zeros(len(field_names), dtype = np.int)
+    for pt in data_dict:
+        for i in range(len(field_names)):
+            field_name = field_names[i]
+            if pt[field_name] != '':
+                field_counts[i] += 1
+    out = []
+    for i in range(len(field_counts)):
+        if float(field_counts[i]) / float(len(data_dict)) > min_appearance_percentage:
+            out.append(field_names[i])
+
+    return out
+
+def read_fields(csv, field_row):
+    row_num = 0
+    for row in csv:
+        if row_num == field_row:
+            out = []
+            for x in row:
+                out.append(x)
+            return out
+        row_num += 1
+    return None
+
+def clean_datasets(source_csv_path, ignore_rows, field_row, min_appearance_percentage, output_writer):
+    field_names = read_fields(csv.reader(open(source_csv_path)), field_row)
+    data_dict = peter_csv_ops.get_data_as_dicts(csv.reader(open(source_csv_path)), ignore_rows, field_names)
+    clean_fields = get_clean_fields(data_dict, field_names, min_appearance_percentage)
+    def clean_remap_func(row_num, row_dict):
+        if row_num in ignore_rows:
+            return None
+        for clean_field in clean_fields:
+            #if clean_field not in row_dict:
+            if row_dict[clean_field] == '':
+                return None
+
+        return {clean_field: row_dict[clean_field] for clean_field in clean_fields}
+    peter_csv_ops.csv_map(source_csv_path, field_names, clean_remap_func, output_writer)
+
+
+if __name__ == "__main__":
+
+    SOURCE_CSV_PATH = os.path.join(os.getcwd(),  '../..', 'datasets', 'analytic_data2019.csv')
+    END_CSV_PATH = os.path.join(os.getcwd(),  '../..', 'datasets', 'super_clean_analytic_data2019.csv')
+    IGNORE_ROWS = [0,1]
+    FIELD_ROW = 0
+    OUTPUT_WRITER = open(END_CSV_PATH, "w")
+    clean_datasets(SOURCE_CSV_PATH, IGNORE_ROWS, FIELD_ROW, 0.95, OUTPUT_WRITER)
---
+++ linreg.py
@@ -0,0 +1,143 @@
+import csv
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+import numpy as np
+import csvs.csv_ops as csv_ops
+import pandas as pd
+
+
+def open_csv(path):
+    data_csv_file = open(path)
+    return csv.reader(data_csv_file, delimiter=',')
+
+
+def read_fields(csv, field_row):
+    row_num = 0
+    for row in csv:
+        if row_num == field_row:
+            out = []
+            for x in row:
+                out.append(x)
+            return out
+        row_num += 1
+    return None
+
+
+def get_data_as_dicts(csv, ignore_rows, field_names):
+    row_num = 0
+    D = []
+    for row in csv:
+        if not row_num in ignore_rows:
+            i = 0
+            d = {}
+            for x in row:
+                d[field_names[i]] = x
+                i += 1
+            D.append(d)
+        row_num += 1
+    return D
+
+
+def is_numeric_string(s):
+    return s.replace('.', '', 1).isdigit()
+
+
+def get_remove_fields(data_dict, field_names):
+    remove_features = []
+    for d in data_dict:
+        for feature in d:
+            if feature not in remove_features and not is_numeric_string(d[feature]):
+                remove_features.append(feature)
+
+    for field_name in field_names:
+        if field_name not in remove_features and \
+            ("numerator" in field_name
+                or "denominator" in field_name
+                or "FIPS" in field_name
+                or "Year" in field_name
+                or "CI" in field_name):
+            remove_features.append(field_name)
+
+    return remove_features
+
+
+def trim_features(data_dict, remove_features):
+    for remove_feature in remove_features:
+        for i in range(len(data_dict)):
+            del data_dict[i][remove_feature]
+
+    return data_dict
+
+
+def data_dict_to_dataset(data_dict, label_field_name):
+    X_field_order = []
+    for d_feature in data_dict[0]:
+        if d_feature != label_field_name:
+            X_field_order.append(d_feature)
+
+    X = np.zeros((len(data_dict), len(data_dict[0]) - 1), dtype=np.float64)
+
+    y = np.zeros(len(data_dict), dtype=np.float64)
+    for i in range(len(data_dict)):
+        d = data_dict[i]
+        for j in range(len(X_field_order)):
+            X[i, j] = float(d[X_field_order[j]])
+        y[i] = float(d[label_field_name])
+
+    return X, y, X_field_order
+
+
+def get_remove_rows(csv, field_names):
+    row_num = 0
+    remove_rows = [0]
+    for row in csv:
+        if row[1] == '000':
+            remove_rows.append(row_num)
+        row_num += 1
+    return remove_rows
+
+
+DATA_PATH = os.path.join(os.getcwd(),  '..', 'datasets', 'super_clean_analytic_data2019.csv')
+FIELD_NAMES = read_fields(open_csv(DATA_PATH), 0)
+REMOVE_ROWS = get_remove_rows(open_csv(DATA_PATH), FIELD_NAMES)
+DATA_DICT = get_data_as_dicts(open_csv(DATA_PATH), REMOVE_ROWS, FIELD_NAMES)
+REMOVE_FIELDS = get_remove_fields(DATA_DICT, FIELD_NAMES)
+DATA_DICT = trim_features(DATA_DICT, REMOVE_FIELDS)
+X, y, X_field_order = data_dict_to_dataset(DATA_DICT, "Premature age-adjusted mortality raw value")
+
+X = pd.DataFrame(data=X, columns=X_field_order)
+y = pd.DataFrame(data=y, columns=["Mortality Ratio"])
+# print(X)
+
+X -= np.min(X, axis=1)[:, np.newaxis]
+X /= np.max(X, axis=1)[:, np.newaxis]
+
+y -= y.min()
+y /= y.max()
+# y *= 10
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+y_train = y_train.iloc[:, 0]
+y_test = y_test.iloc[:, 0]
+# clf = LogisticRegression(penalty = 'l2', C = 1000.0).fit(X_train, y_train)
+# clf = Lasso(alpha=.000000001, fit_intercept=True)  # l1
+# clf = Ridge(alpha=1.0, fit_intercept=True)  # l2
+clf = LinearRegression()
+clf = clf.fit(X_train, y_train)
+
+for i in range(len(X_field_order)):
+    print((X_field_order[i], clf.coef_[i]))
+
+pred_y = clf.predict(X_test)  # [:,0]
+print('mean absolute error', mean_absolute_error(y_test, pred_y))
+#  clf.score(X_test, y_test)
+# print(('prediction', 'mortality ratio'))
+# for i in range(20):
+#     print((np.array(pred_y)[i], np.array(y_test)[i]))
+
+print("bias", clf.intercept_)
