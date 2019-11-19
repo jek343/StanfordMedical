a0476d194bb6ef48ce5d3b4eafabf4051875b5c6
Jenna <jek343@cornell.edu>
2019-11-19 10:51:19 -0500

    include num and denom

--- CI_dict_creator.py
+++ CI_dict_creator.py
@@ -7,15 +7,25 @@ def get_CI_dict(year):
     CI_dict = {}
     dataset = pd.read_csv(os.path.join(os.getcwd(),  '..', 'datasets', 'super_clean_analytic_data' + str(year) + '.csv'))
     features = list(dataset)
-    features = [i[:-7] for i in features if "CI low" in i]
+    features = [i[:-10] for i in features if "raw value" in i]
+    features_CI = [i[:-7] for i in features if "CI low" in i]
+    features_num = [i[:-10] for i in features if "numerator" in i]
 
     for index, row in dataset.iterrows():
         row_dict = {}
         fips = row["5-digit FIPS Code"]
         for feat in features:
-            low = row[feat + " CI low"]
             actual = row[feat + " raw value"]
-            high = row[feat + " CI high"]
-            row_dict[feat] = (low, actual, high)
+            low = None
+            high = None
+            numerator = None
+            denom = None
+            if feat in features_CI:
+                low = row[feat + " CI low"]
+                high = row[feat + " CI high"]
+            elif feat in features_num:
+                numerator = row[feat + " numerator"]
+                denom = row[feat + " denominator"]
+            row_dict[feat] = (low, actual, high, numerator, denom)
         CI_dict[fips] = row_dict
     return CI_dict
