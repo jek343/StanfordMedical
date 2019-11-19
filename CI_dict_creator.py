e3f967da9ca40e3996384936e2bcd455df9d8066
Jenna <jek343@cornell.edu>
2019-11-19 11:52:25 -0500

    fixed

--- CI_dict_creator.py
+++ CI_dict_creator.py
@@ -7,25 +7,34 @@ def get_CI_dict(year):
     CI_dict = {}
     dataset = pd.read_csv(os.path.join(os.getcwd(),  '..', 'datasets', 'super_clean_analytic_data' + str(year) + '.csv'))
     features = list(dataset)
-    features = [i[:-10] for i in features if "raw value" in i]
+    features_raw = [i[:-10] for i in features if "raw value" in i]
     features_CI = [i[:-7] for i in features if "CI low" in i]
+    # print(*features_CI, sep = "\n")
     features_num = [i[:-10] for i in features if "numerator" in i]
+    # print(*features_CI, sep = "\n")
 
     for index, row in dataset.iterrows():
         row_dict = {}
         fips = row["5-digit FIPS Code"]
-        for feat in features:
+        for feat in features_raw:
             actual = row[feat + " raw value"]
             low = None
             high = None
             numerator = None
             denom = None
             if feat in features_CI:
-                low = row[feat + " CI low"]
-                high = row[feat + " CI high"]
+                if feat + " CI low" in features:
+                    low = row[feat + " CI low"]
+                if feat + " CI high" in features:
+                    high = row[feat + " CI high"]
             elif feat in features_num:
-                numerator = row[feat + " numerator"]
-                denom = row[feat + " denominator"]
+                if feat + " numerator" in features:
+                    numerator = row[feat + " numerator"]
+                if feat + " denominator" in features:
+                    denom = row[feat + " denominator"]
             row_dict[feat] = (low, actual, high, numerator, denom)
         CI_dict[fips] = row_dict
     return CI_dict
+
+# dict = get_CI_dict(2019)
+# print(dict[1001])
